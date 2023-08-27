a99a59c935fb9f55da16503e0052aa9e3f7eb652
Daniel Farah <danielfarah54@gmail.com>
2023-08-26 22:45:40 -0300

    add model validation

--- Models/Movies.cs
+++ Models/Movies.cs
@@ -1,8 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MoviesApi.Models;
 
 public class Movie
 {
+  [Required(ErrorMessage = "Please provide a title for the movie")]
+  [MaxLength(100, ErrorMessage = "The title cannot be longer than 100 characters")]
   public string Title { get; set; }
+
+  [Required(ErrorMessage = "Please provide a genre for the movie")]
+  [MaxLength(50, ErrorMessage = "The genre cannot be longer than 50 characters")]
   public string Genre { get; set; }
+
+  [Required(ErrorMessage = "Please provide a length for the movie")]
+  [Range(70, 600, ErrorMessage = "The length must be between 70 and 600 minutes")]
   public int Length { get; set; }
 }
