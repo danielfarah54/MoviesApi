fd671a21a163e4e76bcd28c61d0d0b98954aa6e7
Daniel Farah <danielfarah54@gmail.com>
2023-08-28 20:20:31 -0300

    change controller to use the database

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using MoviesApi.Models;
+using MoviesApi.Data;
 
 namespace MoviesApi.Controllers;
 
@@ -7,11 +8,36 @@ namespace MoviesApi.Controllers;
 [Route("[controller]")]
 public class MoviesController : ControllerBase
 {
-  private static List<Movie> movies = new List<Movie>();
+  private MovieContext _context;
+
+  public MoviesController(MovieContext context)
+  {
+    _context = context;
+  }
 
   [HttpPost]
-  public void AddMovie([FromBody] Movie movie)
+  public IActionResult AddMovie([FromBody] Movie movie)
   {
-    movies.Add(movie);
+    _context.Movies.Add(movie);
+    _context.SaveChanges();
+    return CreatedAtAction(nameof(GetMovieById), new { id = movie.Id }, movie);
+  }
+
+  [HttpGet]
+  public IEnumerable<Movie> GetMovies([FromQuery] int page = 1)
+  {
+    int pageSize = 5;
+    return _context.Movies.Skip((page - 1) * pageSize).Take(pageSize);
+  }
+
+  [HttpGet("{id}")]
+  public IActionResult GetMovieById(int id)
+  {
+    var movie = _context.Movies.FirstOrDefault(movie => movie.Id == id);
+    if (movie == null)
+    {
+      return NotFound();
+    }
+    return Ok(movie);
   }
 }
